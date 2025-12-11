8070a83ac72e8139085086d2247afd5e4002a1a6
batulan187 <batulan187@gmail.com>
2025-12-11 15:07:53 +0800

    Initial commit - Laravel routing activity

---
+++ resources/views/contactpage.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact Page</title>
+</head>
+<body>
+    <h1>Contact Us Here</h1>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('homepage');
+});
+Route::get('/contact', function () {
+    return view('contactpage');
+});
+Route::get('/about', function () {
+    return view('aboutpage');
+});
